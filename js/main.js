0438a1060648fadebf896dcce58be38cbbc95ff7
Cortland Klein <me@pixelcort.com>
2012-07-11 15:53:12 -0700

    No need to convert to Date objects and back again.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
 App.ready = function main() {
 	App.initialize();
 
-	for (var i=0,l=window.TEST_FEED.length;i<l;i++) {
-		var entry = window.TEST_FEED[i];
-		entry.timestamp_ms = Date.parse(entry.timestamp);
-	}
-
 	App.crossfilter = crossfilter(window.TEST_FEED);
 
 	App.dimension_kind = App.crossfilter.dimension(function(d){return d.kind;});
@@ -17,20 +12,12 @@ App.ready = function main() {
 	});
 	App.getPath('router.kindsController').set('content', kinds);
 
-	App.dimension_timestamp_ms = App.crossfilter.dimension(function(d){return d.timestamp_ms;});
-	App.group_timestamp_ms_by_day = App.dimension_timestamp_ms.group(function(timestamp_ms) {
-		var date = new Date();
-		date.setTime(timestamp_ms);
-
-		var year = date.getFullYear()+'';
-		var month = date.getMonth()+1+'';
-		if (month.length==1) month = '0'+month;
-		var date = date.getDate()+'';
-		if (date.length==1) date = '0'+date;
-		return year+'-'+month+'-'+date;
+	App.dimension_timestamp = App.crossfilter.dimension(function(d){return d.timestamp;});
+	App.group_timestamp_by_day = App.dimension_timestamp.group(function(timestamp) {
+		return timestamp.slice(0,10)
 	});
 	var days = [];
-	App.group_timestamp_ms_by_day.all().forEach(function(day) {
+	App.group_timestamp_by_day.all().forEach(function(day) {
 		days.pushObject(App.Day.create({
 			day: day.key
 		}));
