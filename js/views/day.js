ba539db96ead5a63663dc3b79f9a96db97844a9c
Cortland Klein <me@pixelcort.com>
2012-07-11 21:16:40 -0700

    Filtering based on Kind

--- js/views/day.js
+++ js/views/day.js
@@ -1,3 +1,23 @@
 App.DayView = Em.View.extend({
+	selectedKindBinding: 'App.router.kindsController.selection',
+
+	filteredDatebookGroups: function() {
+		var selectedKind = this.get('selectedKind');
+
+		if (!selectedKind) return this.getPath('context.datebookGroups');
+		debugger;
+		return this.getPath('context.datebookGroups').filter(function(g){return g.get('kind') === selectedKind;});
+	}.property('context.datebookGroups', 'selectedKind'),
+	filteredHistoricalGroups: function() {
+		var selectedKind = this.get('selectedKind');
+
+		if (!selectedKind) return this.getPath('context.historicalGroups');
+		return this.getPath('context.historicalGroups').filter(function(g){return g.get('kind') === selectedKind;});
+	}.property('context.historicalGroups', 'selectedKind'),
+
+	shouldShow: function() {
+		return this.getPath('filteredDatebookGroups.length') || this.getPath('filteredHistoricalGroups.length');
+	}.property('filteredDatebookGroups.length', 'filteredHistoricalGroups.length'),
+
 	templateName: 'day'
 });
