e5ae47b5a857f6f4af041fb0bd78855dd53fb51b
Cortland Klein <me@pixelcort.com>
2012-07-11 20:54:35 -0700

    Whoops, left in a debugger.

--- js/models/group.js
+++ js/models/group.js
@@ -27,7 +27,6 @@ App.Group = Em.Object.extend({
 					case 'campaign': return "Complete \"%@1\"Campaign".loc(activity.reference.campaign.name);
 					case 'call_list':
 						var length = activity.reference.call_list.users.length;
-						if (length===0) debugger;
 						return (length===1)?"Call 1 User".loc():"Call %@1 Users".loc(length);
 					case 'todo': return "Complete TODO: %@1".loc(activity.reference.todo.description);
 					case 'deal': return "Close the \"%@1\" Deal".loc(activity.reference.deal.name);
